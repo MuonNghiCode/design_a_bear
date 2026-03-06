64ef0e82b652bd25e8eb033181c0ca08bebb2629
MuonNghiCode <minhquanpm1610@gmail.com>
2026-03-06 22:19:56 +0700

    feat: implement custom scrollbar component with drag support and visibility controls

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -2,7 +2,13 @@
 
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ReactNode } from "react";
+import CustomScrollbar from "@/components/shared/CustomScrollbar";
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      {children}
+      <CustomScrollbar />
+    </AuthProvider>
+  );
 }
